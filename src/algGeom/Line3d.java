582e6a9b271c86133adfbe459610f1208533bef0
JavierPin <javierpinels@gmail.com>
2016-03-08 14:23:36 +0100

    Distancia punto recta 3D

--- src/algGeom/Line3d.java
+++ src/algGeom/Line3d.java
@@ -6,6 +6,15 @@ public class Line3d extends Edge3d{
 	   super (o,d);
    }
         
+   public double distancia (Vect3d p){
+        Vect3d M, B;
+        B = new Vect3d(orig.x,orig.y,orig.z);
+        //M = new Vector((new Vector(b.x,b.y)).resta(B));
+        M = new Vect3d(dest.x-orig.x,dest.y-orig.y,dest.z-orig.z);
+        double t0 = M.dot(new Vect3d(p.x-B.x,p.y-B.y,p.z-B.z))/M.dot(M);
+        //double t0=M.dot((p.resta(B)))/M.dot(M);
+       return (float)(p.resta(B.suma(M.prodEscalar(t0)))).modulo();
+   }
 	
     /** Muestra en pantalla los valores los valores de Edge3d */
     public void out () {
